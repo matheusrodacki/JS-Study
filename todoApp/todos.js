84100f988d4293d513adc8f082b8a127d76c0282
matheusrodacki <matheus.rodacki@outlook.com>
2019-08-21 20:44:31 -0300

    created delete function

--- todoApp/todos.js
+++ todoApp/todos.js
@@ -11,11 +11,26 @@ var todos = [
 
 function renderTodos() {
     listElement.innerHTML = '';
+
     for (todo of todos){
         var todoElement = document.createElement('li');
         var todoText =  document.createTextNode(todo);
 
+        var linkElement = document.createElement('a');
+        
+        linkElement.setAttribute('href', '#');
+
+        var pos = todos.indexOf(todo);
+        linkElement.setAttribute('onclick', 'deleteTodo(' + pos + ')');
+
+
+        var linkText = document.createTextNode('Excuir');
+
+        linkElement.appendChild(linkText);
+
         todoElement.appendChild(todoText);
+        todoElement.appendChild(linkElement);
+
         listElement.appendChild(todoElement);
     }
 }
@@ -31,3 +46,10 @@ function addTodo(){
 }
 
 buttonElement.onclick = addTodo;
+
+function deleteTodo(pos) {
+    todos.splice(pos, 1);
+
+    console.log("Removido item "+pos);
+    renderTodos();
+}
