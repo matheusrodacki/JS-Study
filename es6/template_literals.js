0c2350ae827b869d75155b0210a028093a63e57d
matheusrodacki <matheus.rodacki@outlook.com>
2019-08-22 12:12:31 -0300

    create template_literals

---
+++ es6/template_literals.js
@@ -0,0 +1,7 @@
+// TEMPLATE LITERALS
+const nome = 'Diego';
+const idade = '23';
+
+//console.log('Meu nome é '+nome+' e tenho '+idade+' anos');
+
+console.log(`Meu nome é ${nome} e tenho ${idade} anos!`);
