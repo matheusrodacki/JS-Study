8c618dfde2f168eaa4e77fd9fa239156d98f3ca8
matheusrodacki <matheus.rodacki@outlook.com>
2019-08-22 23:31:13 -0300

    render on screen with js

--- es6/src/main.js
+++ es6/src/main.js
@@ -3,6 +3,7 @@ class App {
         this.repositories = [];
 
         this.formEl = document.getElementById('repo-form');
+        this.listEl = document.getElementById('repo-list');
 
         this.registerHandlers();
     }
@@ -23,9 +24,37 @@ class App {
 
         });
 
-        console.log(this.repositories);
+        this.render();
+    }
+
+    render() {
+        this.listEl.innerHTML = '';
+
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let descEl = document.createElement('p');
+            descEl.appendChild(document.createTextNode(repo.description));
+
+            let linkEl =  document.createElement('a');
+            linkEl.setAttribute('target', '_blank');
+            linkEl.appendChild(document.createTextNode('Acessar'));
+
+            let listItemEl =  document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl);
+        });
 
     }
+
 }
 
 new App();
