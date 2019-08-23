af2a065a0ad403c3792bea5987d8f115caef4a22
matheusrodacki <matheus.rodacki@outlook.com>
2019-08-22 22:12:50 -0300

    create class app and addRepository function

--- es6/src/main.js
+++ es6/src/main.js
@@ -1,17 +1,32 @@
-import axios from 'axios';
+class App {
+    constructor() {
+        this.repositories = [];
 
-class Api{
-    static async getUserInfo(username){
-        try{
-            const response = await axios.get(`http://api.github.com/users/${username}`);
+        this.formEl = document.getElementById('repo-form');
 
-            console.log(response);
-        } catch (err){
-            console.warn('Erro na API');
-        }
-       
+        this.registerHandlers();
+    }
+
+    registerHandlers() {
+        this.formEl.onsubmit = event => this.addRepository(event);
+
+    }
+
+    addRepository(event) {
+        event.preventDefault();
+
+        this.repositories.push({
+            name: 'rocketseat.com.br',
+            description: 'Tire a sua idéia do papele dê vida à sua startup',
+            avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
+            html_url: 'http://github.com/rocketseat/rocketseat.com.br',
+
+        });
+
+        console.log(this.repositories);
+
     }
 }
 
-Api.getUserInfo('matheusrodacki');
-Api.getUserInfo('diego3g');
+new App();
+
